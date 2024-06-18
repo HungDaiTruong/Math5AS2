3c2a372a56b5edc151b3ff5058ed31c7df77fcad
Sarra Koussaier <95942823+sarrakou@users.noreply.github.com>
2024-06-18 17:29:11 +0200

    manuallyextrude

--- Assets/BezierAssets/Scripts/ExtrusionAxe.cs
+++ Assets/BezierAssets/Scripts/ExtrusionAxe.cs
@@ -56,7 +56,7 @@ public class ExtrusionAxe : MonoBehaviour
         mesh.vertices = vertices;
         mesh.uv = uvs;
         mesh.triangles = new int[0]; // Initially, no triangles
-        mesh.RecalculateNormals();
+        mesh.ManuallyRecalculateNormals();
 
         // Apply the new color to the material
         if (meshRenderer == null)
