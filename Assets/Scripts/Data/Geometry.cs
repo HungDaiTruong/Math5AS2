81a2dc2593cb6db45a5e2288c607631a812c3fb7
Esilff <72709130+Esilff@users.noreply.github.com>
2025-06-25 11:00:57 +0200

    add ToMesh and GetCube method

--- Assets/Scripts/Data/Geometry.cs
+++ Assets/Scripts/Data/Geometry.cs
@@ -59,6 +59,18 @@ namespace Data
 
             IsClosed = IsMeshClosed();
 
+            foreach (Edge edge in Edges)
+            {
+                if (!Vertices.Contains(edge.StartPoint))
+                {
+                    Vertices.Add(edge.StartPoint);
+                }
+
+                if (!Vertices.Contains(edge.EndPoint))
+                {
+                    Vertices.Add(edge.EndPoint);
+                }
+            }
         }
 
         public List<Edge> Edges { get; private set; }
@@ -111,13 +123,79 @@ namespace Data
 
         public static Geometry GetCube()
         {
+            List<Vertex> vertices = new List<Vertex>()
+            {
+                new Vertex(new Vector3(-0.5f, -0.5f, -0.5f)), // 0: back-left-bottom
+                new Vertex(new Vector3( 0.5f, -0.5f, -0.5f)), // 1: back-right-bottom
+                new Vertex(new Vector3( 0.5f, -0.5f,  0.5f)), // 2: front-right-bottom
+                new Vertex(new Vector3(-0.5f, -0.5f,  0.5f)), // 3: front-left-bottom
+
+                new Vertex(new Vector3(-0.5f,  0.5f, -0.5f)), // 4: back-left-top
+                new Vertex(new Vector3( 0.5f,  0.5f, -0.5f)), // 5: back-right-top
+                new Vertex(new Vector3( 0.5f,  0.5f,  0.5f)), // 6: front-right-top
+                new Vertex(new Vector3(-0.5f,  0.5f,  0.5f))  // 7: front-left-top
+            };
+            
             List<Face> faces = new List<Face>
             {
-                //Add actual geometry
+                new Face(new List<Edge>() {new Edge(vertices[6],vertices[7]), new Edge(vertices[7], vertices[3]), new Edge(vertices[3], vertices[6])}),
+                new Face(new List<Edge>() {new Edge(vertices[6],vertices[3]), new Edge(vertices[3], vertices[2]), new Edge(vertices[2], vertices[6])}),
+                
+                new Face(new List<Edge>() {new Edge(vertices[4],vertices[5]), new Edge(vertices[5], vertices[1]), new Edge(vertices[1], vertices[4])}),
+                new Face(new List<Edge>() {new Edge(vertices[4],vertices[1]), new Edge(vertices[1], vertices[0]), new Edge(vertices[0], vertices[4])}),
+                
+                new Face(new List<Edge>() {new Edge(vertices[5],vertices[6]), new Edge(vertices[6], vertices[2]), new Edge(vertices[2], vertices[5])}),
+                new Face(new List<Edge>() {new Edge(vertices[5],vertices[2]), new Edge(vertices[2], vertices[1]), new Edge(vertices[1], vertices[5])}),
+                
+                new Face(new List<Edge>() {new Edge(vertices[7],vertices[4]), new Edge(vertices[4], vertices[0]), new Edge(vertices[0], vertices[7])}),
+                new Face(new List<Edge>() {new Edge(vertices[7],vertices[0]), new Edge(vertices[0], vertices[3]), new Edge(vertices[3], vertices[7])}),
+                
+                new Face(new List<Edge>() {new Edge(vertices[5],vertices[4]), new Edge(vertices[4], vertices[7]), new Edge(vertices[7], vertices[5])}),
+                new Face(new List<Edge>() {new Edge(vertices[5],vertices[7]), new Edge(vertices[7], vertices[6]), new Edge(vertices[6], vertices[5])}),
+                
+                new Face(new List<Edge>() {new Edge(vertices[2],vertices[3]), new Edge(vertices[3], vertices[1]), new Edge(vertices[1], vertices[2])}),
+                new Face(new List<Edge>() {new Edge(vertices[3],vertices[0]), new Edge(vertices[0], vertices[1]), new Edge(vertices[1], vertices[3])}),
             };
             Geometry cube = new Geometry(faces);
 
             return cube;
+        }
+
+        public Mesh ToMesh()
+        {
+            Mesh mesh = new Mesh();
+            
+            List<Vertex> vertices = new List<Vertex>();
+            List<int> triangles = new List<int>();
+
+            foreach (Face face in Faces)
+            {
+                List<int> usedIndices = new List<int>();
+
+                foreach (Vertex vertex in face.Vertices)
+                {
+                    var idx = vertices.FindIndex(v => v.Value == vertex.Value);
+                    if (idx == -1)
+                    {
+                        vertices.Add(vertex);
+                        triangles.Add(vertices.Count - 1);
+                        usedIndices.Add(vertices.Count - 1);
+                    }
+                    else if (!usedIndices.Contains(idx))
+                    {
+                        triangles.Add(idx);
+                        usedIndices.Add(idx);
+                    }
+                }
+            }
+
+            mesh.vertices = vertices.Select(v => v.Value).ToArray();
+            mesh.triangles = triangles.ToArray();
+
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            return mesh;
         }
     }
 
